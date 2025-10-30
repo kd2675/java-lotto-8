1f3f9f7e18c19e80304904ee3ee6692787849fda
김도영 <zz01064842675@gmail.com>
2025-10-30 11:28:41 +0900

    feat: 로또 입력 및 유효성 검사 기능 추가
    - Input 클래스 생성 및 사용자 입력 처리 메서드 구현
    - Lotto 클래스에 번호 범위와 중복 검사 로직 추가

---
+++ src/main/java/lotto/Input.java
@@ -0,0 +1,15 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class Input {
+    public static final String INPUT_MONEY_MESSAGE = "구입금액을 입력해 주세요.";
+    public static final String INPUT_WINNING_NUMBER_MESSAGE = "당첨 번호를 입력해 주세요.";
+    public static final String INPUT_BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
+
+    public static String input(String message) {
+        System.out.println(message);
+
+        return Console.readLine();
+    }
+}
--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -7,6 +7,8 @@ public class Lotto {
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        validateRange(numbers);
+        validateDuplicate(numbers);
         this.numbers = numbers;
     }
 
@@ -17,4 +19,21 @@ public class Lotto {
     }
 
     // TODO: 추가 기능 구현
+    private void validateRange(List<Integer> numbers) {
+        for (int number : numbers) {
+            validateRange(number);
+        }
+    }
+
+    private void validateRange(int number) {
+        if (number < 1 || number > 45) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 1-45 사이 숫자이어야합니다.");
+        }
+    }
+
+    private void validateDuplicate(List<Integer> numbers) {
+        if (numbers.size() != numbers.stream().distinct().count()) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복될 수 없습니다.");
+        }
+    }
 }
